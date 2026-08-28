cb011fa7d947591362591ed46fed8a6d7278f21c
Mehedi Hasan Herock <mehedi@mirinfosys.com>
2026-08-28 14:29:04 +0600

    Initial Commit

---
+++ AllChrono/AllChronoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllChronoApp.swift
+//  AllChrono
+//
+//  Created by Herock Hasan on 8/28/26.
+//
+
+import SwiftUI
+
+@main
+struct AllChronoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
